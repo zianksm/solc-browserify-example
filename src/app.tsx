df697d07af45e69819c61661e7e72ed2d5511113
zian <mynameiszian@gmail.com>
2023-01-04 13:33:40 +0800

    done styling editor

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Editor from "react-simple-code-editor";
+import Prism from "prismjs";
+import "./prism.css";
+// TODO build simple editor with syntax highlighting.
 
 function App() {
+  Prism.manual = true;
+  const [code, setCode] = React.useState(
+    `\n//Tell the Solidity compiler what version to use
+pragma solidity ^0.4.8;
+
+//Declares a new contract
+contract SimpleStorage {
+    //Storage. Persists in between transactions
+    uint x;
+
+    //Allows the unsigned integer stored to be changed
+    function set(uint newValue) {
+        x = newValue;
+    }
+    
+    //Returns the currently stored unsigned integer
+    function get() returns (uint) {
+        return x;
+    }
+}`
+  );
+
+  const _highlight = (code: string) => {
+    const html = Prism.highlight(code, Prism.languages.js, "js");
+    Prism.highlightAll();
+    return html;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container-main">
+      <h1 id="headline">Solc In the Broswer!</h1>
+      <div id="code-container">
+        <h3 id="depedencies-disclaimer">
+          *support openzeppelin depedencies(wip)
+        </h3>
+        <Editor
+          className="code-style-container"
+          id="text-code"
+          highlight={(code) => _highlight(code)}
+          value={code}
+          onValueChange={(code) => setCode(code)}
+          tabSize={4}
+          preClassName="language-solidity"
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+        ></Editor>
+      </div>
+      <button id="button-compile">compile</button>
     </div>
   );
 }
